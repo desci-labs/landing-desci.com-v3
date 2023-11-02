9258bbcc13ffc8ac72f21986d753ea9a9b98fad8
carlaost <ostmanncarla@gmail.com>
2023-11-02 17:32:14 +0100

    Styling and copy updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import { Markdown } from '@astro/types';
+
+
 export default defineConfig({
    site: 'https://lexingtonthemes.com',
-  integrations: [tailwind(),  sitemap()]
+  integrations: [tailwind(),  sitemap()],
+  renderers: [Markdown],
 });
